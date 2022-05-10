e0919a17116d2003c41c216bb7cdd0d5560edbaf
Julie Nisbet <jmnisbet@gmail.com>
2022-05-10 11:04:54 -0700

    supabase demo

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,43 +1,47 @@
-const SUPABASE_URL = "https://njovlynmvtgkoexzepbb.supabase.co";
+const SUPABASE_URL = 'https://nwxkvnsiwauieanvbiri.supabase.co';
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTMxNjU3MSwiZXhwIjoxOTU2ODkyNTcxfQ.4sFYy_xhbdh7hVgaEmH_ILGTIl6rsqtUgzAJsizIxkA";
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzAwMzQzNCwiZXhwIjoxOTUyNTc5NDM0fQ.8XIsU0FANdaNeQnT-DojpTL-GTlTPZ4CYZDEetpFpWc';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 function checkError({ data, error }) {
-  return error ? console.error(error) : data;
+    return error ? console.error(error) : data;
 }
 
 export async function getMovies() {
-  // return the list of all movies
-  const resp = await client.from("movies").select("*");
-  return checkError(resp);
+    // return the list of all movies
+    const resp = await client.from('movies').select('*');
+    console.log(resp);
+    return checkError(resp);
 }
 
 export async function getMoviesWithDirector() {
-  // return the list of all the movies with their director
+    // return the list of all the movies with their director
+    const resp = await client.from('movies').select('title, directors(name)');
+    console.log(resp);
+    return checkError(resp);
 }
 
 export async function getDirectorNames() {
-  // return the list of the director's names
+    // return the list of the director's names
 }
 
 export async function getMovieById(id) {
-  // return the movie with the given id
+    // return the movie with the given id
 }
 
 export async function getMovieByTitle(title) {
-  // return the movie with the given title
+    // return the movie with the given title
 }
 
 export async function getOldestMovie() {
-  // return the oldest movie (assume the database is not sorted)
+    // return the oldest movie (assume the database is not sorted)
 }
 
 export async function getMoviesAfter(year) {
-  // return movies made after the year passed in
+    // return movies made after the year passed in
 }
 
 export async function getHighestGrossingMovie() {
-  // return movie with the highest box office total
+    // return movie with the highest box office total
 }
